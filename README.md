1ea18afcf99c2dc1aaf0aa63abce2fa85c4f6be2
Keith Huntington <github-kjh@wizardfish.com>
2023-02-10 15:41:35 -0500

    Updated with Collections
    -

--- README.md
+++
@@ -1,2 +0,0 @@
-# KJH-Audible-Library
- 
--- data/temp-data.1676057927781.js
+++
@@ -1,1 +0,0 @@
-window.tempDataJSON = {"books":true,"series":true,"collections":false,"wishlist":false,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1676057927781}};
