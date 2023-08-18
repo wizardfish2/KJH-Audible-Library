f192abc26d65bf0cda3562d8e364843b5529461e
Keith Huntington <github-kjh@wizardfish.com>
2023-08-18 10:50:50 -0400

    (2017)
    more

--- assets/gallery-timeStringToSeconds.9aa0cb6a.js
+++
@@ -1,1 +0,0 @@
-var r={methods:{timeStringToSeconds:function(t){if(t&&t.match(/\d/)){const c=t.match(/[0-9]( ?)+(m|min|分)/),n=t.match(/\d+/g),e={},s=function(m){return+m*60*60},o=function(m){return+m*60};return n.length===2?(e.h=n[0],e.m=n[1],e.numbers=s(e.h)+o(e.m)):c?(e.m=n[0],e.numbers=o(e.m)):(e.h=n[0],e.numbers=s(e.h)),e.numbers}else return 0}}};export{r as t};
