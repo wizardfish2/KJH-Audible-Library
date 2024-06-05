904735744d7fc215bf53a12b1d2a4e0e5f632d03
Keith Huntington <github-kjh@wizardfish.com>
2024-06-05 13:00:32 -0400

    (3030)
    3030 : 48305 hours =( 5 y, 06 m, 07 d, 17 h)

--- data/temp-data.1717592199697.js
+++
@@ -1,1 +0,0 @@
-window.tempDataJSON = {"books":true,"series":true,"collections":false,"podcasts":false,"wishlist":false,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":true},{"key":"authors","enabled":true},{"key":"publishers","enabled":true},{"key":"series","enabled":true},{"key":"narrators","enabled":true}],"cacheID":1717592199697}};
