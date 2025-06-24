a003bdf6f865407aa9f16400e25f98aced993b3b
Keith Huntington <github-kjh@wizardfish.com>
2025-06-23 20:41:29 -0400

    2025-06-23 : Total=4209, (Purchased=4140, PlusLib=69), 65563 hours =( 7 y, 05 m, 26 d, 19 h)
    2025-06-23 : Total=4209, (Purchased=4140, PlusLib=69), 65563 hours =( 7 y, 05 m, 26 d, 19 h)

---
+++ data/split-book-data/1974986632.1750724957235.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p>Someone sent three fully armed ships to High Tortuga to back the slave-owners running the mines, and Barnabas and Shinigami are determined to find out who.</p>"; 
