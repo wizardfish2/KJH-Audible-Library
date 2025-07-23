fba1f844e520720801fdcc14f57a6fb9994d496f
Keith Huntington <github-kjh@wizardfish.com>
2025-07-23 12:22:35 -0400

    2025-07-23 : Total=4223, (Purchased=4161, PlusLib=62), 65866 hours =( 7 y, 06 m, 09 d, 10 h)
    2025-07-23 : Total=4223, (Purchased=4161, PlusLib=62), 65866 hours =( 7 y, 06 m, 09 d, 10 h)

---
+++ data/split-book-data/1974987515.1753287435781.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p>When Barnabas discovers one of the biggest players in his sector, he vows to bring them down. But can he handle an entire fleet bearing down on him?</p>"; 
---
+++ data/split-book-data/1974991997.1753287435781.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p>Barnabas has made some enemies - enemies who want to track him down through space and kill him. But he's more than ready to take on this challenge.</p>"; 
