eaca2689471b3025b8f257ff8c7b7f535ebc5ca1
Keith Huntington <github-kjh@wizardfish.com>
2025-09-02 12:34:04 -0400

    2025-09-02 : Total=4498, (Purchased=4483, PlusLib=15), 70401 hours =( 8 y, 00 m, 13 d, 09 h)
    2025-09-02 : Total=4498, (Purchased=4483, PlusLib=15), 70401 hours =( 8 y, 00 m, 13 d, 09 h)

--- data/split-book-data/B0BVDVSJMR.1756481718835.js
+++
@@ -1,1 +0,0 @@
-window.bookSummaryJSON = "<p>Book 4 of the bestselling Fantasy LitRPG Series by Seth Ring, author of Nova Terra. Get your copy today and explore a world whose secrets have been buried under decades of war.</p>"; 
