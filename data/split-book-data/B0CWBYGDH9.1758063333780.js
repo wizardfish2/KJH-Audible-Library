78c12b9418faaab944d6b67fc072acb994f9b75d
Keith Huntington <github-kjh@wizardfish.com>
2025-09-17 13:12:41 -0400

    2025-09-18 : Total=4556, (Purchased=4551, PlusLib=05), 71527 hours =( 8 y, 02 m, 00 d, 07 h)
    2025-09-18 : Total=4556, (Purchased=4551, PlusLib=05), 71527 hours =( 8 y, 02 m, 00 d, 07 h)

--- data/split-book-data/B0CWBYGDH9.1758063333780.js
+++
@@ -1,1 +0,0 @@
-window.bookSummaryJSON = "<p><b>If she's this powerful, then I can't help but wonder about the others...</b></p> <p>The Saga of Nate continues!</p> <p>A dangerous bloodline and family, a powerful arch nemesis, S-ranked dungeon, and disciples with secrets of their own...</p> <p>Something big is happening behind the scenes.</p> <p>Welcome to Rise of the Cheat Potion Maker 5!</p>"; 
