899e208fe5bf5899006ae784eac5fe949ed035b9
Keith Huntington <20329723+wizardfish2@users.noreply.github.com>
2026-02-17 21:23:38 -0500

    2026-02-17 : Total=5094, (Purchased=5093), 78733 hours =( 8 y, 12 m, 00 d, 13 h)
    2026-02-17 : Total=5094, (Purchased=5093), 78733 hours =( 8 y, 12 m, 00 d, 13 h)

---
+++ data/split-book-data/B0DDMNXCRB.1771380322911.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p><b>They asked Amber to become a magical girl three times.</b></p> <p>She refused. That only made her more interesting.</p> <p>The fourth time, she asked them.</p>"; 
