214aa64d299baf0c9262444e34fba33ab0a796ba
Keith Huntington <20329723+wizardfish2@users.noreply.github.com>
2026-03-25 20:18:32 -0400

    2026-03-25 : Total=5180, (Purchased=5179), 79991 hours =( 9 y, 01 m, 17 d, 23 h)
    2026-03-25 : Total=5180, (Purchased=5179), 79991 hours =( 9 y, 01 m, 17 d, 23 h)

---
+++ data/split-book-data/B09CF14FKD.1774483857006.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p><b>Elaine's adventures continue at Ranger Academy! </b></p> <p>Will she pass and validate Julius' trust in her? Or will she fail out and need to find a new career?</p>"; 
