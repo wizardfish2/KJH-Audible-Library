7ab6ee16a0683ae2e9969ee404889ca9735db409
Keith Huntington <20329723+wizardfish2@users.noreply.github.com>
2026-04-01 09:36:53 -0400

    2026-04-01 : Total=5322, (Purchased=5208), 81535 hours =( 9 y, 03 m, 22 d, 07 h)
    2026-04-01 : Total=5322, (Purchased=5208), 81535 hours =( 9 y, 03 m, 22 d, 07 h)

---
+++ data/split-book-data/B0F68KTN8G.1775050144111.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p>Dale's worlds are facing unprecedented dangers from horrors beyond the scope of understanding. Facing threats beyond the power of gods, all creation is at risk.</p>"; 
